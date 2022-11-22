bb043236e756832870682c8e111e71a95b95b236
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2022-11-23 00:09:12 +0100

    Add basing docstring for a crate.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+//! ### Benchmark harness with few too many knobs sticking out
+//!
+//! This crate provides a library for benchmarking user-defined scenarios using
+//! [Callgrind](https://valgrind.org/docs/manual/cl-manual.html), a call-graph and cache prediction
+//! profiler. It aims to serve both upcoming and present benchmarking gurus.
+//! Whenever possible, terminology/naming of calliper aligns with that of Callgrind (in i.e.
+//! parameter names).
 mod benchmark_setup;
 mod callgrind;
 pub mod utils;
