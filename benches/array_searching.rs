977d9a39649a06bf670ffd6f8158d6783e4a0f82
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2022-11-24 00:57:57 +0100

    Add new benchmark for arrat searching.

---
+++ benches/array_searching.rs
@@ -0,0 +1,34 @@
+use calliper::utils::black_box;
+use calliper::{run, ScenarioConfig, Scenario};
+
+#[inline(never)]
+#[no_mangle]
+fn binary_search_impl(haystack: &[u8], needle: u8) -> Result<usize, usize> {
+    haystack.binary_search(&needle)
+    
+}
+fn bench_binary_search() {
+    let range = (0..255).collect::<Vec<_>>();
+    let _ = black_box(binary_search_impl(black_box(&range), black_box(253)));
+}
+
+#[inline(never)]
+#[no_mangle]
+fn linear_search_impl(haystack: &[u8], needle: u8) -> Option<usize> {
+    haystack.iter().position(|n| *n == needle)
+}
+
+fn bench_linear_search() {
+    let range = (0..255).collect::<Vec<_>>();
+    black_box(linear_search_impl(black_box(&range), black_box(253)));
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let instance = ScenarioConfig::default();
+    let benches = [
+        Scenario::new(bench_linear_search, &instance),
+        Scenario::new(bench_binary_search, &instance),
+    ];
+    run(&benches).unwrap();
+    Ok(())
+}
