d223725cbd5da6ed9b675937ff99f2bd47957321
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2023-01-23 00:11:57 +0100

    doc: Change sentence.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 /*!### Benchmark harness with few too many knobs sticking out
 
- This crate provides a library for benchmarking user-defined scenarios using
+ Calliper is a library for benchmarking with
  [Callgrind](https://valgrind.org/docs/manual/cl-manual.html), a call-graph and cache prediction
  profiler. It aims to serve both upcoming and present benchmarking gurus.
  Whenever possible, terminology/naming of Calliper aligns with that of Callgrind (in i.e.
