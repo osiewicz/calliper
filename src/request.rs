f43d4807ce3462d0d6a6f92e8061d805ba1133a9
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2023-03-26 01:30:59 +0100

    Use crabgrind instead of self-rolled impl

--- src/request.rs
+++ src/request.rs
@@ -1,13 +1,5 @@
 //! Callgrind requests module for fine-grained control over Callgrind behavior.
-pub mod aarch64;
-///
-/// Our implementation is based on edef's valgrind_request crate that does not work with more
-/// recent Rust versions.
-pub mod x86_64;
-#[cfg(target_arch = "aarch64")]
-use aarch64 as host;
-#[cfg(target_arch = "x86_64")]
-use x86_64 as host;
+use crabgrind::callgrind;
 
 /// Valgrind client request.
 #[non_exhaustive]
@@ -26,15 +18,12 @@ impl ClientRequest {
     ///
     /// Under the hood this emits a noop code sequence that's recognized by Valgrind as an escape
     /// hatch. It has no effect in normal runs of a program.
+    #[inline(always)]
     pub fn now(self) {
-        const BASE: u64 = ((b'C' as u64) << 24) + ((b'T' as u64) << 16);
-        let id = match self {
-            Self::ToggleCollection => BASE + 2,
-            Self::StartInstrumentation => BASE + 4,
-            Self::StopInstrumentation => BASE + 5,
-        };
-        unsafe {
-            host::do_client_request(0, &[id, 0, 0, 0, 0, 0]);
+        match self {
+            Self::ToggleCollection => callgrind::toggle_collect(),
+            Self::StartInstrumentation => callgrind::start_instrumentation(),
+            Self::StopInstrumentation => callgrind::stop_instrumentation(),
         }
     }
 }
